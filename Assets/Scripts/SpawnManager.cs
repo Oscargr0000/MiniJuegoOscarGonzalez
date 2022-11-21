ec6d73e61fb2991ee21c07e34b7edc58062c707e
Oscargr0000 <91455089+Oscargr0000@users.noreply.github.com>
2022-11-21 16:16:50 +0100

    dasdas

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] fruits;
-    public GameObject[] fruitsPlus;
+
+    private bool spawnON;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnON = true;
+        StartCoroutine(Spawn(2));
+        Debug.Log("Screen Width : " + Screen.width);
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Spawn(float waitTime)
     {
-        
+
+        while (spawnON.Equals(true))
+        {
+            int randomNum = Random.Range(0, fruits.Length);
+
+            Vector2 position = new Vector2(Random.Range(-5, 10), 4);
+
+            Instantiate(fruits[randomNum], position, transform.rotation);
+
+            yield return new WaitForSeconds(waitTime);
+        }
+       
     }
+
 }
