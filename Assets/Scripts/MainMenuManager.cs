cd39bdb106b28cb08f780cf35e4c1f8dcd5a5755
Oscargr0000 <oscargr0000@gmail.com>
2022-12-02 21:52:51 +0100

    asdsad

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -9,14 +9,29 @@ public class MainMenuManager : MonoBehaviour
 
     public TextMeshProUGUI totalPoints;
 
+    public GameObject Shop;
+    public GameObject Menu;
+    public TextMeshProUGUI alert;
+    public Animator _animator;
+
+    private AudioManager Am;
+
     private bool dash;
     private bool dobleJump;
     private bool TimeStop;
 
 
+    private void Awake()
+    {
+        Am = FindObjectOfType<AudioManager>();
+    }
+
+
     private void Start()
     {
         totalPoints.text = PlayerPrefs.GetInt("currentPoints").ToString();
+        Shop.SetActive(false);
+        Menu.SetActive(true);
     }
 
     private void Update()
@@ -33,16 +48,21 @@ public class MainMenuManager : MonoBehaviour
         {
             PlayerPrefs.SetInt("currentPoints", PlayerPrefs.GetInt("currentPoints") - price);
 
-            if (price.Equals(500))
+            if (price.Equals(500) && dobleJump.Equals(false))
             {
                 dobleJump = true;
-                Debug.Log("tienes el salto");
-            }else if (price.Equals(1500))
+
+            }else if (price.Equals(1500)&& dash.Equals(false))
             {
                 dash = true;
-            }else if (price.Equals(4000))
+            }else if (price.Equals(4000) && TimeStop.Equals(false))
             {
                 TimeStop = true;
+            }
+            else
+            {
+                Debug.Log("funciona");
+                StartCoroutine(Fade());
             }
         }
 
@@ -52,5 +72,32 @@ public class MainMenuManager : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadScene(1);
+
+        Am.PLaySound(0);
+    }
+
+    public void GoShop()
+    {
+        Shop.SetActive(true);
+        Menu.SetActive(false);
+        Am.PLaySound(1);
+    }
+
+    public void GoMenu()
+    {
+        Shop.SetActive(false);
+        Menu.SetActive(true);
+        Am.PLaySound(2);
+    }
+
+    IEnumerator Fade()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            Color c = alert.color;
+            c.a = ft;
+            alert.color = c;
+            yield return null;
+        }
     }
 }
